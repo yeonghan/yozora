7d67d6df10ae34117892f2026b14965d41ff8dc0
yeonghan.woo <dudgks29@naver.com>
2020-02-25 00:42:32 +0900

    02

--- first_django/blog/forms.py
+++ first_django/blog/forms.py
@@ -3,9 +3,14 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
-    author = forms.CharField(label='Author', max_length=100, required=True)
-    content = forms.CharField(label='Content',widget=forms.Textarea, required=True)
-
+    author = forms.CharField(
+            label='Author', max_length=100, required=True,
+            widget=forms.TextInput(attrs={'class': 'form-control'})
+            )
+    content = forms.CharField(
+            label='Content', required=True,
+            widget=forms.Textarea(attrs={'class': 'form-control'})
+            )
     class Meta:
         model = Comment
         fields = ['author', 'content']
